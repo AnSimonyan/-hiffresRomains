1d2f0b318269d948dc327e33af316dd035eed432
AnSimonyan <anna.simona1976@gmail.com>
2024-06-08 16:14:23 +0300

    Добавьте файлы проекта.

---
+++ СhiffresRomains/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace СhiffresRomains
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] matchArrayRom = new char[7] { "I", "V", "X", "L", "C", "D", "M" };
+            int[] matchArrayInd = new int[7] { 1,5,10,50,100,500,1000};
+
+
+        }
+    }
+}
